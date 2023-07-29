54c94a4efc769c6ecd908d35d188b70bcc9be608
dejmedus <email96123@gmail.com>
2023-07-29 16:40:59 -0600

    add IGame and IStore

---
+++ lib/types.ts
@@ -0,0 +1,32 @@
+export interface IGame {
+  internalName: string;
+  title: string;
+  metacriticLink: string;
+  dealID: string;
+  storeID: string;
+  gameID: string;
+  salePrice: string;
+  normalPrice: string;
+  isOnSale: string;
+  savings: string;
+  metacriticScore: string;
+  steamRatingText: string;
+  steamRatingPercent: string;
+  steamRatingCount: string;
+  steamAppID: string;
+  releaseDate: number;
+  lastChange: number;
+  dealRating: string;
+  thumb: string;
+}
+
+export interface IStore {
+  storeID: string;
+  storeName: string;
+  isActive: number;
+  images: {
+    banner: string;
+    logo: string;
+    icon: string;
+  };
+}

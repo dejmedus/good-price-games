8fd45d62dfe6354bcde1bc6f52431bcea418ad21
dejmedus <email96123@gmail.com>
2023-07-29 23:16:37 -0600

    add combined game+store and storeURL

--- lib/types.ts
+++ lib/types.ts
@@ -30,3 +30,40 @@ export interface IStore {
     icon: string;
   };
 }
+
+export interface IGameData {
+  internalName: string;
+  title: string;
+  metacriticLink: string;
+  dealID: string;
+  storeID: string;
+  gameID: string;
+  salePrice: string;
+  normalPrice: string;
+  isOnSale: string;
+  savings: string;
+  metacriticScore: string;
+  steamRatingText: string;
+  steamRatingPercent: string;
+  steamRatingCount: string;
+  steamAppID: string;
+  releaseDate: number;
+  lastChange: number;
+  dealRating: string;
+  thumb: string;
+  storeData: {
+    storeID: string;
+    storeUrl: string | null;
+    storeName: string;
+    isActive: number;
+    images: {
+      banner: string;
+      logo: string;
+      icon: string;
+    };
+  };
+}
+
+export interface IStoreURLs {
+  [name: string]: string | null;
+}

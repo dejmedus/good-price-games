4fc6e86f60233b48fec7cf0b2ba1ff1d338c6150
dejmedus <email96123@gmail.com>
2023-07-29 23:14:19 -0600

    increase refresh time & combine game data and store data

--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,27 +1,34 @@
 import NodeCache from "node-cache";
-import { IGame, IStore } from "@/lib/types";
+import { IGame, IStore, IGameData } from "@/lib/types";
 import { columns } from "./Columns";
 import { DataTable } from "./Data-Table";
+import storeUrls from "@/lib/stores";
 
-const cache = new NodeCache({ stdTTL: 600 }); // Cache data for 10 minutes (600 seconds)
+const cache = new NodeCache({ stdTTL: 2400 });
 
-async function getData(): Promise<IGame[]> {
+async function getData(): Promise<IGameData[]> {
   try {
     // check for cached data
-    const cachedData = cache.get<IGame[]>("gameData");
+    const cachedData = cache.get<IGameData[]>("gameData");
     if (cachedData) {
       console.log("Data retrieved from cache");
       return cachedData;
     }
 
     const res = await fetch(
-      "https://www.cheapshark.com/api/1.0/deals?onSale=1&pageSize=240",
+      "https://www.cheapshark.com/api/1.0/deals?onSale=1&pageSize=800",
       {
         method: "GET",
         redirect: "follow",
       }
     );
     const data: IGame[] = await res.json();
+
+    const storeRes = await fetch("https://www.cheapshark.com/api/1.0/stores", {
+      method: "GET",
+      redirect: "follow",
+    });
+    const storeData: IStore[] = await storeRes.json();
 
     // only show the lowest salePrice copy of each game
     const uniqueGames = Object.values(
@@ -37,10 +44,30 @@ async function getData(): Promise<IGame[]> {
       }, {})
     );
 
-    // cache data
-    cache.set("gameData", uniqueGames);
+    // create map of store data
+    const storeMap: { [storeID: string]: IStore } = {};
+    storeData.forEach((store) => {
+      storeMap[store.storeID] = store;
+    });
 
-    return uniqueGames;
+    // create game data objects
+    const gameData: IGameData[] = [];
+    uniqueGames.forEach((game) => {
+      const storeData = storeMap[game.storeID];
+      if (storeData) {
+        const gameInfo: IGameData = {
+          ...game,
+          // add url to store data
+          storeData: { ...storeData, storeUrl: storeUrls[storeData.storeName] },
+        };
+        gameData.push(gameInfo);
+      }
+    });
+
+    // cache data
+    cache.set("gameData", gameData);
+
+    return gameData;
   } catch (err) {
     console.error("Could not fetch games", err);
     return [];
@@ -48,7 +75,7 @@ async function getData(): Promise<IGame[]> {
 }
 
 export default async function DemoPage() {
-  const data = await getData();
+  const data: IGameData[] = await getData();
 
   return (
     <div className="container py-10 mx-auto">

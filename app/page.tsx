08b184e4e635560b5c7f3fd323a2d0da5f56b8cb
dejmedus <email96123@gmail.com>
2023-07-29 23:18:42 -0600

    update hero

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import Table from "@/components/Table/Table";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen py-12">
-      <h1>Good Price Games</h1>
+    <main className="flex flex-col items-center justify-between min-h-screen pt-12 pb-4">
+      <h2 className="text-2xl font-semibold">Good Price Games</h2>
+      <p className="text-neutral-600 dark:text-neutral-400">
+        Find the best sales on the best games
+      </p>
       <Table />
     </main>
   );
